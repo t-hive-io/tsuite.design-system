ad1e053944fdc8721eb7c0774b3c444c51601906
sejrm0 <sejrm0@users.noreply.github.com>
2026-01-03 12:02:10 +0000

    deploy: 212bfc1ae356a3998b70b161a8a424fc4fc0ba8e

---
+++ assets/Layerlefticon.stories-CDoLuP9k.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-DHuurbBU.js";import{r as t}from"./index-uubelm5h.js";const i=e=>t.createElement("svg",{width:24,height:24,viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",...e},t.createElement("path",{d:"M13.9038 12.4038L10.0961 16.2115V8.59615L13.9038 12.4038Z",fill:"#222222"})),f=e=>t.createElement("svg",{width:24,height:24,viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",...e},t.createElement("path",{d:"M12 14.3077L8.19229 10.5H15.8077L12 14.3077Z",fill:"#222222"})),T=({Expanded:e,Visible:n}={Expanded:"False",Visible:"True"})=>e==="False"&&n==="True"?o(i,{}):e==="True"&&n==="True"?o(f,{}):o(i,{}),w={title:"3. Component Category Pages/Tree View/Layer left icon",component:T,parameters:{layout:"centered",docs:{description:{component:"⚠️ **Figma Description Missing** - Please add a description in Figma for this component."}},figma:{pageId:"5200:798",componentId:"1522:9585",figmaComponent:"Layer left icon"}},tags:["autodocs"],argTypes:{Expanded:{control:"select",options:["True","False"],description:'Matches Figma "Expanded" property'},Visible:{control:"select",options:["True","False"],description:'Matches Figma "Visible" property'}},args:{Expanded:"False",Visible:"True"}},r={},s={args:{Expanded:"False",Visible:"True"}},a={args:{Expanded:"True",Visible:"True"}};var p,d,c;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var l,m,u;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    Expanded: 'False',
+    Visible: 'True'
+  }
+}`,...(u=(m=s.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var g,x,E;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    Expanded: 'True',
+    Visible: 'True'
+  }
+}`,...(E=(x=a.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};const V=["Default","NotExpandedVisible","ExpandedVisible"];export{r as Default,a as ExpandedVisible,s as NotExpandedVisible,V as __namedExportsOrder,w as default};
---
+++ assets/Obstacle.stories-73C0ENCL.js
@@ -0,0 +1,16 @@
+import{j as a}from"./jsx-runtime-DHuurbBU.js";import{r as e}from"./index-uubelm5h.js";const l=t=>e.createElement("svg",{width:540,height:152,viewBox:"0 0 540 152",fill:"none",xmlns:"http://www.w3.org/2000/svg",...t},e.createElement("rect",{width:528,height:140,transform:"translate(6 6)",fill:"#D9D9D9"})),b=t=>e.createElement("svg",{width:540,height:152,viewBox:"0 0 540 152",fill:"none",xmlns:"http://www.w3.org/2000/svg",...t},e.createElement("rect",{x:7,y:7,width:526,height:138,fill:"#D9D9D9"}),e.createElement("rect",{x:7,y:7,width:526,height:138,stroke:"#2537F3",strokeWidth:2,strokeDasharray:"6 6"})),x=t=>e.createElement("svg",{width:540,height:152,viewBox:"0 0 540 152",fill:"none",xmlns:"http://www.w3.org/2000/svg",...t},e.createElement("rect",{x:6.5,y:6.5,width:527,height:139,fill:"#D9D9D9"}),e.createElement("rect",{x:6.5,y:6.5,width:527,height:139,stroke:"#2537F3"}),e.createElement("rect",{x:267.5,y:2.5,width:7,height:7,fill:"white",stroke:"#F325A7"}),e.createElement("rect",{x:267.5,y:142.5,width:7,height:7,fill:"white",stroke:"#F325A7"}),e.createElement("rect",{x:2.5,y:72,width:7,height:7,fill:"white",stroke:"#F325A7"}),e.createElement("rect",{x:530.5,y:72,width:7,height:7,fill:"white",stroke:"#F325A7"})),O=({State:t,Type:c}={State:"Default",Type:"Obstical"})=>t==="Default"&&c==="Obstical"?a(l,{}):t==="Flow"&&c==="Obstical"?a(b,{}):t==="Resize"&&c==="Obstical"?a(x,{}):a(l,{}),v={title:"3. Component Category Pages/Canvas Objects/Obstacle",component:O,parameters:{layout:"centered",docs:{description:{component:"⚠️ **Figma Description Missing** - Please add a description in Figma for this component."}},figma:{pageId:"5170:45525",componentId:"2921:846",figmaComponent:"Obstacle"}},tags:["autodocs"],argTypes:{State:{control:"select",options:["Resize","Default","Flow"],description:'Matches Figma "State" property'},Type:{control:"select",options:["Obstical"],description:'Matches Figma "Type" property'}},args:{State:"Default",Type:"Obstical"}},r={},s={args:{State:"Default",Type:"Obstical"}},o={args:{State:"Flow",Type:"Obstical"}},i={args:{State:"Resize",Type:"Obstical"}};var n,p,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var h,g,d;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    State: 'Default',
+    Type: 'Obstical'
+  }
+}`,...(d=(g=s.parameters)==null?void 0:g.docs)==null?void 0:d.source}}};var w,f,u;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    State: 'Flow',
+    Type: 'Obstical'
+  }
+}`,...(u=(f=o.parameters)==null?void 0:f.docs)==null?void 0:u.source}}};var y,S,D;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    State: 'Resize',
+    Type: 'Obstical'
+  }
+}`,...(D=(S=i.parameters)==null?void 0:S.docs)==null?void 0:D.source}}};const T=["Default","DefaultState","FlowState","ResizeState"];export{r as Default,s as DefaultState,o as FlowState,i as ResizeState,T as __namedExportsOrder,v as default};
